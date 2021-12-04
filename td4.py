c989f99b58a8f3c305ce5c34d2935acbfac95e63
Rainer Strotmann <rainerstrotmann@Air-von-Rainer.localdomain>
2021-12-04 11:20:01 +0100

    td4 with basic functionality

--- td4.py
+++ td4.py
@@ -25,8 +25,8 @@ def main(file, debug, fontsize):
 	canvas.set_font_size(fontsize)
 
 	with open(infile) as f:
-		td = Trialdesign(js=json.load(f))
-	td.dump(canvas)
+		td = Trialdesign(js=json.load(f), debug=debug)
+	# td.dump(canvas)
 	td.render(canvas)
 
 def textwidth(canvas, text):
@@ -81,22 +81,6 @@ def administration_names(trial):
 				out += [proc['caption']]
 	return(list(set(out)))
 
-	#return(list(set([c['caption'] for p in trial['periods'] for c in p['administrations']])))	
-
-
-
-
-# def extract_procedure(period, caption):
-# 	temp = []
-# 	for proc in period['procedures']:
-# 		if proc['caption'] == caption:
-# 			if 'times' in proc.keys():
-# 				t = proc['times']
-# 			else: 
-# 				t = [0]
-# 			temp += [(d, t) for d in decode_daylist(proc['days'])]
-# 	return(temp)
-
 def extract_procedure(period, caption):
 	temp = []
 	for x in ['procedures', 'administrations']:
@@ -139,9 +123,6 @@ def period_day_widths1(period, canvas):
 def period_width(period, canvas, width_function):
 	return(sum(width_function(period, canvas)))
 
-# def period_widths(trial, canvas, width_function):
-# 	return([period_width(p, canvas, width_function) for p in trial['periods']])
-
 def period_day_starts(period, canvas, xoffset, width_function):
 	out=[xoffset]
 	acc = xoffset
@@ -156,11 +137,13 @@ def period_day_centers(period, canvas, xoffset, width_function):
 
 #### RENDERING ####
 
-def render_daygrid(period, canvas, xoffset, yoffset, height, width_function):
+def render_daygrid(period, canvas, xoffset, yoffset, height, width_function, debug=False):
 	canvas.save()
 	canvas.set_line_width(1.2)
 	canvas.set_source_rgb(0, 0, 0)
 	y = yoffset
+	if debug:
+		render_dummy(period, canvas, xoffset, yoffset, height, width_function)
 	for start, width, center, label, shading in zip(period_day_starts(period, canvas, xoffset, width_function), width_function(period, canvas), period_day_centers(period, canvas, xoffset, width_function), day_labels(period), day_shadings(period)):
 		if shading:
 			canvas.rectangle(start, y, width, height)
@@ -175,10 +158,20 @@ def render_daygrid(period, canvas, xoffset, yoffset, height, width_function):
 	canvas.restore()
 	return(height)
 
-def render_dummy(period, canvas, caption, xoffset, yoffset, lineheight, width_function, default_symbol):
+def render_periodcaption(period, canvas, xoffset, yoffset, height, width_function, debug=False):
+	canvas.save()
+	if debug:
+		render_dummy(period, canvas, xoffset, yoffset, height, width_function)
+	xcenter = xoffset + period_width(period, canvas, width_function)/2
+	canvas.move_to(xcenter - textwidth(canvas, str(period['caption']))/2, yoffset+ height - (height-textheight(canvas, "X"))/2)
+	canvas.show_text(str(period['caption']))
+	canvas.restore()
+	return(height)
+
+def render_dummy(period, canvas, xoffset, yoffset, lineheight, width_function):
 	canvas.save()
 	canvas.set_source_rgb(random.uniform(0.8, 1), random.uniform(0.8, 1), random.uniform(0.8, 1))
-	canvas.rectangle(xoffset, yoffset, period_width(period, canvas,width_function), lineheight)
+	canvas.rectangle(xoffset, yoffset, period_width(period, canvas, width_function), lineheight)
 	canvas.fill()
 	canvas.restore()
 	return
@@ -186,7 +179,7 @@ def render_dummy(period, canvas, caption, xoffset, yoffset, lineheight, width_fu
 def render_procedure(period, canvas, caption, xoffset, yoffset, lineheight, width_function, default_symbol, debug=False):
 
 	if debug:
-		render_dummy(period, canvas, caption, xoffset, yoffset, lineheight, width_function, default_symbol)
+		render_dummy(period, canvas, xoffset, yoffset, lineheight, width_function)
 	y = yoffset + lineheight/2 # center of the line
 	canvas.move_to(5, y + textheight(canvas, caption) * (1/2 - 0.1))
 	canvas.show_text(caption)
@@ -260,78 +253,65 @@ def draw_arrow(canvas, x, y, height, width, size=1.2):
 	canvas.stroke()
 	canvas.restore()
 
-# def draw_dummy(canvas, x_start, x_end, y):
-# 	canvas.save()
-# 	canvas.set_source_rgb(random.uniform(0.8, 1), random.uniform(0.8, 1), random.uniform(0.8, 1))
-# 	height = textheight(canvas, "X")
-# 	canvas.rectangle(x_start, y-height/2, x_end - x_start, y+height/2)
-# 	canvas.fill()
-# 	canvas.restore()
-
 
 
 class Trialdesign(object):
-	def __init__(self, js=dict()):
+	def __init__(self, js=dict(), debug=True):
 		self._periods = []
 		for p in js["periods"]:
 			self._periods.append(p)
 		self._td = js
+		self._debug = debug
 
-	def render(self, canvas):
+	def render(self, canvas, width_function=period_day_widths):
 		ypadding = 7
-		ygap = textheight(canvas, "X")
+		ylabelpadding = 3
+		yheaderpadding = ypadding
+
 		periodspacing = textwidth(canvas, "XX"	)
+		headerheight = textheight(canvas, "X") * 2
 		lineheight = textheight(canvas, "X") * 1.5
 
 		xoffset = max([textwidth(canvas, i) for i in procedure_names(self._td)]) + 30
 		yoffset = 10
+
+		## render header:
+		def render_periods(x, y, height, render_function):
+			for p in self._periods:
+				yy = render_function(p, canvas, x, y, height, width_function, debug=self._debug)
+				x += period_width(p, canvas, width_function) + periodspacing
+			return(y + yy)
+
 		x = xoffset
+		y = yoffset
+		y = render_periods(xoffset, y, headerheight, render_periodcaption) + ylabelpadding
+		y = render_periods(xoffset, y, headerheight, render_daygrid) + yheaderpadding
+
+		# render dashes between periods
+		x = xoffset
+		w = [period_width(i, canvas, width_function) for i in self._periods]
+		for i in w[:-1]:
+			x += i
+			canvas.move_to(x, y-yheaderpadding - headerheight/2)
+			canvas.line_to(x + periodspacing, y-yheaderpadding-headerheight/2)
+			x += periodspacing
+
 		def render_items(name_function, default_symbol, y):
 			for n in name_function(self._td):
 				x = xoffset
 				for p in self._periods:
-					yy = render_procedure(p, canvas, n, x, y, lineheight, period_day_widths, default_symbol, debug=False)
-					x += period_width(p, canvas, period_day_widths) + periodspacing
+					yy = render_procedure(p, canvas, n, x, y, lineheight, width_function, default_symbol, debug=self._debug)
+					x += period_width(p, canvas, width_function) + periodspacing
 				y += yy + ypadding
 			return(y)
 
-		# header
-		headerheight = textheight(canvas, "X") * 2
-		for p in self._periods:
-			yy = render_daygrid(p, canvas, x, yoffset, headerheight, period_day_widths)
-			x += period_width(p, canvas, period_day_widths) + periodspacing
-		xx = xoffset
-		w = [period_width(i, canvas, period_day_widths) for i in self._periods]
-		for i in w[:-1]:
-			xx += i
-			canvas.move_to(xx, yoffset+headerheight/2)
-			canvas.line_to(xx + periodspacing, yoffset+headerheight/2)
-			xx += periodspacing
-		y = yoffset + yy + ygap
-
-		# procedures
+		# render procedures
 		for nf, ds in [(administration_names, "arrow"), (procedure_names, "diamond")]:
 			y = render_items(nf, ds, y)
 
 
 	def dump(self, canvas):
 		for p in self._periods:
-			# for i in p['administrations']:
-			# print(extract_procedure(p, 'itraconazole'))
-			# 	print(i)
-
-			# print(f'period width: {period_width(p, canvas, period_day_widths)}')
-			# print(f'day widths: {period_day_widths(p, canvas)}')
-			# print(f'day starts: {period_day_starts(p, canvas, 100, period_day_widths)}')
-			# #print(f'day starts: {period_day_starts(p, canvas, 100, period_day_widths1)}')
-			# print(f'day centers: {period_day_centers(p, canvas, 100, period_day_widths)}')
-			# print(f'day labels: {day_labels(p)}')
-			# print(f'day shading: {day_shadings(p)}')
-			# #print(decode_daylist([1, "2", "3-6, 8"]))
-			#print(procedure_names(self._td))
-			# print(extract_procedure(p, "PK sampling"))
-			# print(procedure_symbols(p, "PK sampling", default="arrow"))
-			# print(extract_procedure(p, "ECG"))
 			print()
 
 
