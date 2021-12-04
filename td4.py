3beb595f2d20e3b62414bcaf66fcf800ff7ac562
Rainer Strotmann <rainerstrotmann@Air-von-Rainer.localdomain>
2021-12-04 15:54:48 +0100

    rendering refactored

--- td4.py
+++ td4.py
@@ -8,6 +8,7 @@ import random
 # import math
 import re
 import itertools
+#from decorators import safe_render
 
 
 @click.command()
@@ -23,10 +24,13 @@ def main(file, debug, fontsize):
 	canvas = cairo.Context(surface)
 	canvas.select_font_face("Calibri", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
 	canvas.set_font_size(fontsize)
+	canvas.set_line_width(1.2)
+	canvas.set_line_join(cairo.LINE_JOIN_ROUND)
+	canvas.set_line_join(cairo.LINE_CAP_ROUND)
+	canvas.set_source_rgb(0, 0, 0)
 
 	with open(infile) as f:
 		td = Trialdesign(js=json.load(f), debug=debug)
-	# td.dump(canvas)
 	td.render(canvas)
 
 def textwidth(canvas, text):
@@ -137,121 +141,7 @@ def period_day_centers(period, canvas, xoffset, width_function):
 
 #### RENDERING ####
 
-def render_daygrid(period, canvas, xoffset, yoffset, height, width_function, debug=False):
-	canvas.save()
-	canvas.set_line_width(1.2)
-	canvas.set_source_rgb(0, 0, 0)
-	y = yoffset
-	if debug:
-		render_dummy(period, canvas, xoffset, yoffset, height, width_function)
-	for start, width, center, label, shading in zip(period_day_starts(period, canvas, xoffset, width_function), width_function(period, canvas), period_day_centers(period, canvas, xoffset, width_function), day_labels(period), day_shadings(period)):
-		if shading:
-			canvas.rectangle(start, y, width, height)
-			canvas.set_source_rgb(0.85, 0.85, 0.85)
-			canvas.fill()
-			canvas.set_source_rgb(0, 0, 0)
-		canvas.rectangle(start, y, width, height)
-		canvas.stroke()
-		canvas.move_to(center - textwidth(canvas, str(label)) / 2, yoffset + height - (height- textheight(canvas, "X")) / 2)
-		canvas.show_text(str(label))
-		canvas.stroke()
-	canvas.restore()
-	return(height)
 
-def render_periodcaption(period, canvas, xoffset, yoffset, height, width_function, debug=False):
-	canvas.save()
-	if debug:
-		render_dummy(period, canvas, xoffset, yoffset, height, width_function)
-	xcenter = xoffset + period_width(period, canvas, width_function)/2
-	canvas.move_to(xcenter - textwidth(canvas, str(period['caption']))/2, yoffset+ height - (height-textheight(canvas, "X"))/2)
-	canvas.show_text(str(period['caption']))
-	canvas.restore()
-	return(height)
-
-def render_dummy(period, canvas, xoffset, yoffset, lineheight, width_function):
-	canvas.save()
-	canvas.set_source_rgb(random.uniform(0.8, 1), random.uniform(0.8, 1), random.uniform(0.8, 1))
-	canvas.rectangle(xoffset, yoffset, period_width(period, canvas, width_function), lineheight)
-	canvas.fill()
-	canvas.restore()
-	return
-
-def render_procedure(period, canvas, caption, xoffset, yoffset, lineheight, width_function, default_symbol, debug=False):
-
-	if debug:
-		render_dummy(period, canvas, xoffset, yoffset, lineheight, width_function)
-	y = yoffset + lineheight/2 # center of the line
-	canvas.move_to(5, y + textheight(canvas, caption) * (1/2 - 0.1))
-	canvas.show_text(caption)
-	positions = period_day_centers(period, canvas, xoffset, width_function)
-	widths = width_function(period, canvas)
-	symbols = procedure_symbols(period, caption, default_symbol)
-	for p, w, s in zip(positions, widths, symbols):
-		if s != "":
-			render_symbol(canvas, p, y, textheight(canvas, "X"), w, s)
-	return(lineheight)
-
-def render_symbol(canvas, x, y, height, width, symbol):
-	if symbol == "diamond":
-		draw_diamond(canvas, x, y, height)
-	elif symbol == "block":
-		draw_rect(canvas, x, y, height, width)
-	elif symbol == "arrow":
-		draw_arrow(canvas, x, y, height, width)
-	return
-
-def procedure_symbols(period, caption, default="diamond"):
-	temp = extract_procedure(period, caption)
-	out = [""] * period['length']
-	for (d, t) in temp:
-		if len(t) > 1:
-			symbol = "block"
-		else:
-			symbol = default
-		out[day_index(period, d)] = symbol
-	return(out)
-
-def draw_diamond(canvas, x, y, height, size=0.7):
-	canvas.save()
-	canvas.set_line_width(1.2)
-	canvas.set_line_join(cairo.LINE_JOIN_ROUND)
-	canvas.set_line_join(cairo.LINE_CAP_ROUND)
-	canvas.set_source_rgb(0, 0, 0)
-	canvas.move_to(x, y - height*size)
-	canvas.line_to(x + height*1/2*size, y)
-	canvas.line_to(x, y + height*size)
-	canvas.line_to(x - height*1/2*size, y)
-	canvas.line_to(x, y - height*size)
-	canvas.stroke()
-	canvas.restore()
-
-def draw_rect(canvas, x, y, height, width, size=1):
-	h = height * .8 * size
-	w = width * .7 * size
-	canvas.save()
-	canvas.set_line_width(1.2)
-	canvas.set_source_rgb(0, 0, 0)
-	canvas.rectangle(x - w/2, y - h/2,  w, h)
-	canvas.stroke()
-	canvas.restore()	
-
-def draw_arrow(canvas, x, y, height, width, size=1.2):
-	canvas.save()
-	canvas.set_line_width(1.4)
-	canvas.set_line_join(cairo.LINE_JOIN_ROUND)
-	canvas.set_line_join(cairo.LINE_CAP_ROUND)
-	canvas.set_source_rgb(0, 0, 0)
-	ybase = y - height * size / 2
-	yapex = y + height * size / 2
-	yarrow = yapex - (yapex - ybase) * .45
-	xarrow = (yapex - ybase) * .15
-	canvas.move_to(x, ybase)
-	canvas.line_to(x, yapex)
-	canvas.line_to(x - xarrow, yarrow)
-	canvas.move_to(x, yapex)
-	canvas.line_to(x + xarrow, yarrow)
-	canvas.stroke()
-	canvas.restore()
 
 
 
@@ -263,7 +153,8 @@ class Trialdesign(object):
 		self._td = js
 		self._debug = debug
 
-	def render(self, canvas, width_function=period_day_widths):
+	def render(self, cn, width_function=period_day_widths):
+		canvas = cn
 		ypadding = 7
 		ylabelpadding = 3
 		yheaderpadding = ypadding
@@ -275,10 +166,119 @@ class Trialdesign(object):
 		xoffset = max([textwidth(canvas, i) for i in procedure_names(self._td)]) + 30
 		yoffset = 10
 
+		def safe_render(render_func):
+			def wrapper(*args, **kwargs):
+				canvas.save()
+				out = render_func(*args, **kwargs)
+				canvas.stroke()
+				#canvas.fill()
+				canvas.restore()
+				return out
+			return wrapper
+
+		@safe_render
+		def render_daygrid(period, xoffset, yoffset, height, width_function, debug=False):
+			y = yoffset
+			if debug:
+				render_dummy(period, canvas, xoffset, yoffset, height, width_function)
+			for start, width, center, label, shading in zip(period_day_starts(period, canvas, xoffset, width_function), width_function(period, canvas), period_day_centers(period, canvas, xoffset, width_function), day_labels(period), day_shadings(period)):
+				if shading:
+					canvas.rectangle(start, y, width, height)
+					canvas.set_source_rgb(0.85, 0.85, 0.85)
+					canvas.fill()
+					canvas.set_source_rgb(0, 0, 0)
+				canvas.rectangle(start, y, width, height)
+				canvas.stroke()
+				canvas.move_to(center - textwidth(canvas, str(label)) / 2, yoffset + height - (height- textheight(canvas, "X")) / 2)
+				canvas.show_text(str(label))
+				#canvas.stroke()
+			return(height)
+
+		@safe_render
+		def render_periodcaption(period, xoffset, yoffset, height, width_function, debug=False):
+			if debug:
+				render_dummy(period, xoffset, yoffset, height, width_function)
+			xcenter = xoffset + period_width(period, canvas, width_function)/2
+			canvas.move_to(xcenter - textwidth(canvas, str(period['caption']))/2, yoffset+ height - (height-textheight(canvas, "X"))/2)
+			canvas.show_text(str(period['caption']))
+			return(height)
+
+		@safe_render
+		def render_dummy(period, xoffset, yoffset, lineheight, width_function):
+			canvas.set_source_rgb(random.uniform(0.8, 1), random.uniform(0.8, 1), random.uniform(0.8, 1))
+			canvas.rectangle(xoffset, yoffset, period_width(period, canvas, width_function), lineheight)
+			canvas.fill()
+			return
+
+		@safe_render
+		def render_procedure(period, caption, xoffset, yoffset, lineheight, width_function, default_symbol, debug=False):
+			if debug:
+				render_dummy(period, xoffset, yoffset, lineheight, width_function)
+			y = yoffset + lineheight/2 # center of the line
+			canvas.move_to(5, y + textheight(canvas, caption) * (1/2 - 0.1))
+			canvas.show_text(caption)
+			positions = period_day_centers(period, canvas, xoffset, width_function)
+			widths = width_function(period, canvas)
+			symbols = procedure_symbols(period, caption, default_symbol)
+			for p, w, s in zip(positions, widths, symbols):
+				if s != "":
+					render_symbol(p, y, textheight(canvas, "X"), w, s)
+			return(lineheight)
+
+		def draw_line(x1, y1, x2, y2):
+			canvas.move_to(x1, y1)
+			canvas.line_to(x2, y2)
+
+		def render_symbol(x, y, height, width, symbol):
+			if symbol == "diamond":
+				draw_diamond(x, y, height)
+			elif symbol == "block":
+				draw_rect(x, y, height, width)
+			elif symbol == "arrow":
+				draw_arrow(x, y, height, width)
+			return
+
+		def procedure_symbols(period, caption, default="diamond"):
+			temp = extract_procedure(period, caption)
+			out = [""] * period['length']
+			for (d, t) in temp:
+				if len(t) > 1:
+					symbol = "block"
+				else:
+					symbol = default
+				out[day_index(period, d)] = symbol
+			return(out)
+
+		@safe_render
+		def draw_diamond(x, y, height, size=0.7):
+			#canvas.set_line_width(1.2)
+			draw_line(x, y - height*size, x + height*1/2*size, y)
+			canvas.line_to(x, y + height*size)
+			canvas.line_to(x - height*1/2*size, y)
+			canvas.line_to(x, y - height*size)
+			#canvas.stroke()
+
+		@safe_render
+		def draw_rect(x, y, height, width, size=1):
+			h = height * .8 * size
+			w = width * .7 * size
+			canvas.rectangle(x - w/2, y - h/2,  w, h)
+
+		@safe_render
+		def draw_arrow(x, y, height, width, size=1.2):
+			canvas.set_line_width(1.4)
+			ybase = y - height * size / 2
+			yapex = y + height * size / 2
+			yarrow = yapex - (yapex - ybase) * .45
+			xarrow = (yapex - ybase) * .15
+			draw_line(x, ybase, x, yapex)
+			canvas.line_to(x - xarrow, yarrow)
+			draw_line(x, yapex, x + xarrow, yarrow)
+
 		## render header:
 		def render_periods(x, y, height, render_function):
 			for p in self._periods:
-				yy = render_function(p, canvas, x, y, height, width_function, debug=self._debug)
+				yy = render_function(p, x, y, height, width_function, debug=self._debug)
 				x += period_width(p, canvas, width_function) + periodspacing
 			return(y + yy)
 
@@ -292,15 +292,15 @@ class Trialdesign(object):
 		w = [period_width(i, canvas, width_function) for i in self._periods]
 		for i in w[:-1]:
 			x += i
-			canvas.move_to(x, y-yheaderpadding - headerheight/2)
-			canvas.line_to(x + periodspacing, y-yheaderpadding-headerheight/2)
+			draw_line(x, y-yheaderpadding - headerheight/2, x + periodspacing, y-yheaderpadding-headerheight/2)
 			x += periodspacing
+			canvas.stroke()
 
 		def render_items(name_function, default_symbol, y):
 			for n in name_function(self._td):
 				x = xoffset
 				for p in self._periods:
-					yy = render_procedure(p, canvas, n, x, y, lineheight, width_function, default_symbol, debug=self._debug)
+					yy = render_procedure(p, n, x, y, lineheight, width_function, default_symbol, debug=self._debug)
 					x += period_width(p, canvas, width_function) + periodspacing
 				y += yy + ypadding
 			return(y)
